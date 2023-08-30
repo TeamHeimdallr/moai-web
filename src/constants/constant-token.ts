9fee0a8d65010f9cfe466345a824fb893efdcd73
Sean IM <chaaaamni@gmail.com>
2023-08-30 11:21:38 +0900

    update constants

---
+++ src/constants/constant-token.ts
@@ -0,0 +1,29 @@
+import { TokenDAI, TokenMNT, TokenMOAI, TokenUSDC, TokenUSDT, TokenWETH } from '~/assets/images';
+
+export enum TOKEN {
+  DAI = 'DAI',
+  USDC = 'USDC',
+  USDT = 'USDT',
+  MNT = 'MNT',
+  WETH = 'WETH',
+  MOAI = 'MOAI',
+}
+
+export const TOKEN_IMAGE_MAPPER: Record<TOKEN, string> = {
+  DAI: TokenDAI,
+  USDC: TokenUSDC,
+  USDT: TokenUSDT,
+  MNT: TokenMNT,
+  WETH: TokenWETH,
+  MOAI: TokenMOAI,
+};
+
+// TODO: static token price
+export const TOKEN_PRICE_MAPPER: Record<TOKEN, number> = {
+  DAI: 1,
+  USDC: 1,
+  USDT: 1,
+  MNT: 0.4417,
+  WETH: 1718.39,
+  MOAI: 100,
+};
