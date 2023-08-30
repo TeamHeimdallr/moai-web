2238c6cecc94464eb3359aab0a68d970f98a89b4
jihyune <rlawlcns96@gmail.com>
2023-08-30 15:19:00 +0900

    fix: toggle detail size

--- src/components/toggle/_toggle.stories.tsx
+++ src/components/toggle/_toggle.stories.tsx
@@ -1,16 +1,18 @@
-import type { Meta } from '@storybook/react';
-import { useState } from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
 
 import { Toggle } from '.';
 
 const meta = {
-  title: 'Components/Toggle/toggle',
+  title: 'Components/Toggle/Toggle',
+  component: Toggle,
   tags: ['autodocs'],
-} satisfies Meta;
+} satisfies Meta<typeof Toggle>;
 
 export default meta;
+type Story = StoryObj<typeof meta>;
 
-export const _Toggle = () => {
-  const [selected, select] = useState(false);
-  return <Toggle select={() => select(prev => !prev)} selected={selected} />;
+export const _Toggle: Story = {
+  args: {
+    selected: false,
+  },
 };
--- src/components/toggle/index.tsx
+++ src/components/toggle/index.tsx
@@ -4,12 +4,12 @@ import tw, { css, styled } from 'twin.macro';
 import { COLOR } from '~/assets/colors';
 interface Props extends HTMLAttributes<HTMLDivElement> {
   selected: boolean;
-  select: () => void;
+  select?: () => void;
 }
 
-export const Toggle = ({ selected, select }: Props) => {
+export const Toggle = ({ selected, select, ...rest }: Props) => {
   return (
-    <Wrapper selected={selected} onClick={select}>
+    <Wrapper selected={selected} onClick={select} {...rest}>
       <Circle selected={selected} />
     </Wrapper>
   );
@@ -21,7 +21,7 @@ interface DivProps {
 
 const Wrapper = styled.div<DivProps>(({ selected }) => [
   tw`
-    relative flex w-40 h-24 rounded-12 clickable transition-color 
+    relative w-40 h-24 rounded-12 clickable transition-color 
   `,
   selected ? tw`bg-primary-50` : tw`bg-neutral-20 hover:bg-neutral-40`,
   !selected &&
@@ -36,5 +36,5 @@ const Circle = styled.div<DivProps>(({ selected }) => [
   tw`
     absolute rounded-full drop-shadow-default transition-toggle transition-color
   `,
-  selected ? tw`top-2 w-20 h-20 bg-neutral-100 left-18` : tw`top-4 w-16 h-16 bg-neutral-80 left-4`,
+  selected ? tw`top-3 w-19 h-19 bg-neutral-100 left-18` : tw`top-4 w-16 h-16 bg-neutral-80 left-4`,
 ]);
