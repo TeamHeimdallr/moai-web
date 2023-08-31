a8e0e1ac907986df480288a50b85216225630760
lucky7323 <eunho.ambition@gmail.com>
2023-08-31 20:53:40 +0900

    feat: slippage settings

--- src/pages/detail/add-liquidity/components/add-lp-input/index.tsx
+++ src/pages/detail/add-liquidity/components/add-lp-input/index.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import tw from 'twin.macro';
+import { useRef, useState } from 'react';
+import tw, { styled } from 'twin.macro';
+import { useOnClickOutside } from 'usehooks-ts';
 
 import { IconSetting } from '~/assets/icons';
 import { ButtonPrimaryLarge } from '~/components/buttons/primary';
@@ -17,14 +18,18 @@ interface Props {
   tokenList: TokenInfo[];
 }
 export const AddLpInput = ({ tokenList }: Props) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [slippage, setSlippage] = useState(1);
+
   const priceImpact = 0.13; // TODO
   const [inputValue1, setInputValue1] = useState<number>(0);
   const [inputValue2, setInputValue2] = useState<number>(0);
   const [inputValue3, setInputValue3] = useState<number>(0);
 
-  const handleSetting = () => {
-    // TODO
-  };
+  const [opened, open] = useState(false);
+  const toggle = () => open(!opened);
+
+  useOnClickOutside(ref, () => open(false));
 
   const handleMax = () => {
     // TODO
@@ -40,11 +45,32 @@ export const AddLpInput = ({ tokenList }: Props) => {
 
   return (
     <Wrapper>
-      <Header>
+      <Header ref={ref}>
         <Title>Enter liquidity amount</Title>
-        <IconWrapper onClick={() => handleSetting()}>
-          <IconSetting fill="#9296AD" width={20} height={20} />
+        <IconWrapper onClick={toggle}>
+          <IconSetting fill={opened ? '#F5FF83' : '#9296AD'} width={20} height={20} />
         </IconWrapper>
+        {opened && (
+          <SlippageWrapper>
+            <SlippageInnerWarpper>
+              <SlippageText>Slippage tolerance</SlippageText>
+              <SlippageOptions>
+                <SlippageOption selected={slippage === 0} onClick={() => setSlippage(0)}>
+                  {'0.5%'}
+                </SlippageOption>
+                <SlippageOption selected={slippage === 1} onClick={() => setSlippage(1)}>
+                  {'1.0%'}
+                </SlippageOption>
+                <SlippageOption selected={slippage === 2} onClick={() => setSlippage(2)}>
+                  {'2.0%'}
+                </SlippageOption>
+                <SlippageOption selected={slippage === 3} disabled={true}>
+                  {'or enter manually'}
+                </SlippageOption>
+              </SlippageOptions>
+            </SlippageInnerWarpper>
+          </SlippageWrapper>
+        )}
       </Header>
       <InnerWrapper>
         {tokenList &&
@@ -97,8 +123,38 @@ const InnerWrapper = tw.div`
 `;
 
 const IconWrapper = tw.div`
-  clickable w-32 h-32
+  clickable w-32 h-32 items-center justify-center flex
 `;
+
+const SlippageWrapper = tw.div`
+  gap-12 bg-neutral-15 rounded-8 box-shadow-default px-16 py-12 max-w-294 absolute top-60 right-24
+`;
+
+const SlippageInnerWarpper = tw.div`
+  flex flex-col gap-12
+`;
+
+const SlippageText = tw.div`
+  text-neutral-100 font-m-16
+`;
+
+const SlippageOptions = tw.div`
+  flex gap-8 w-full flex-wrap
+`;
+
+interface SlippageOptionProps {
+  selected?: boolean;
+  disabled?: boolean;
+}
+const SlippageOption = styled.div<SlippageOptionProps>(({ selected, disabled }) => [
+  tw`
+  gap-10 px-16 py-6 rounded-8 bg-transparent text-neutral-60 font-r-16 border-solid border-1 border-neutral-60 clickable
+`,
+  !disabled && selected
+    ? tw`text-primary-50 border-primary-50 gradient-chip`
+    : tw`hover:bg-neutral-20 hover:text-neutral-60 border-neutral-80`,
+  disabled && tw`non-clickable`,
+]);
 
 const Title = tw.div`
   text-neutral-100 font-b-16
