cf9741e1be7e7ff66e755d72dad360a31042f4d6
jihyune <rlawlcns96@gmail.com>
2023-09-01 20:00:38 +0900

    fix: type

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,11 +1,12 @@
+import { TOKEN_USD_MAPPER } from '~/constants';
 import { PoolInfo } from '~/types/components';
 import { TOKEN } from '~/types/contracts';
 
 export const pools: PoolInfo[] = [
   {
     compositions: [
-      { name: TOKEN.MOAI, weight: 80, balance: 7077.75 },
-      { name: TOKEN.WETH, weight: 20, balance: 147 },
+      { name: TOKEN.MOAI, weight: 80, balance: 7077.75, price: TOKEN_USD_MAPPER[TOKEN.MOAI] },
+      { name: TOKEN.WETH, weight: 20, balance: 147, price: TOKEN_USD_MAPPER[TOKEN.WETH] },
     ],
     value: '$1,259,280',
     volume: '$78,086',
@@ -16,9 +17,9 @@ export const pools: PoolInfo[] = [
   },
   {
     compositions: [
-      { name: TOKEN.WETH, weight: 50, balance: 147 },
-      { name: TOKEN.USDC, weight: 30, balance: 90 },
-      { name: TOKEN.USDT, weight: 20, balance: 60 },
+      { name: TOKEN.WETH, weight: 50, balance: 147, price: TOKEN_USD_MAPPER[TOKEN.WETH] },
+      { name: TOKEN.USDC, weight: 30, balance: 90, price: TOKEN_USD_MAPPER[TOKEN.USDC] },
+      { name: TOKEN.USDT, weight: 20, balance: 60, price: TOKEN_USD_MAPPER[TOKEN.USDT] },
     ],
     value: '$10,073,500',
     volume: '$458,137',
