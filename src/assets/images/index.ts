8b6afe6e731e125b460724c5e8b6e3e4e28b3308
Sean IM <chaaaamni@gmail.com>
2023-09-14 13:25:46 +0900

    add token images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,7 +1,9 @@
 import BgMain from './bg-main.png';
 import TokenMOAI from './token-moai.png';
+import TokenROOT from './token-root.png';
 import TokenUSDC from './token-usdc.png';
 import TokenUSDT from './token-usdt.png';
 import TokenWETH from './token-weth.png';
+import TokenXRPL from './token-xrpl.png';
 
-export { BgMain, TokenMOAI, TokenUSDC, TokenUSDT, TokenWETH };
+export { BgMain, TokenMOAI, TokenROOT, TokenUSDC, TokenUSDT, TokenWETH, TokenXRPL };
