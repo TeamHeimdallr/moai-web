6ef79eaa97ff8569af9587aea9aea5ac9a9151c9
Sean IM <chaaaamni@gmail.com>
2023-09-14 15:04:14 +0900

    add dropdown list

---
+++ src/components/dropdown/dropdown-list/_dropdown-list.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { TokenROOT } from '~/assets/images';
+
+import { DropdownList } from '.';
+
+const meta = {
+  title: 'Components/Dropdown/DropdownList',
+  component: DropdownList,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof DropdownList>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Normal: Story = {
+  args: {
+    id: 'token-1',
+
+    image: TokenROOT,
+    imageAlt: 'Root',
+    imageTitle: 'Root',
+
+    text: 'Token',
+  },
+};
+
+export const Selected: Story = {
+  args: {
+    id: 'token-1',
+
+    image: TokenROOT,
+    imageAlt: 'Root',
+    imageTitle: 'Root',
+
+    selected: true,
+    handleSelect: id => console.log(id),
+
+    text: 'Token',
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    id: 'token-1',
+
+    image: TokenROOT,
+    imageAlt: 'Root',
+    imageTitle: 'Root',
+
+    disabled: true,
+    selected: true,
+    handleSelect: id => console.log(id),
+
+    text: 'Token',
+  },
+};
