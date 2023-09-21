fc577b4eae84724625d6677be1e326a7515aa664
lucky7323 <eunho.ambition@gmail.com>
2023-09-21 18:40:01 +0900

    feat: add liquidity and swap of xrpl

--- src/pages/xrpl-test/index.tsx
+++ src/pages/xrpl-test/index.tsx
@@ -11,6 +11,16 @@ import { InputTextField } from '~/components/inputs/textfield';
 const XrplTestPage = () => {
   const [xrpValue, setXrpValue] = useState(0);
   const [moaiValue, setMoaiValue] = useState(0);
+
+  const [xrpAddValue, setXrpAddValue] = useState(0);
+  const [moaiAddValue, setMoaiAddValue] = useState(0);
+
+  const [xrpFromValue, setXrpFromValue] = useState(0);
+  const [moaiFromValue, setMoaiFromValue] = useState(0);
+
+  const [xrpToValue, setXrpToValue] = useState(0);
+  const [moaiToValue, setMoaiToValue] = useState(0);
+
   const { checkAmmExist, ammInfo, getFee } = useGetAmmInfo({
     asset1: {
       currency: 'XRP',
@@ -58,6 +68,118 @@ const XrplTestPage = () => {
     }
   };
 
+  const handleProvideLP = async () => {
+    const result = await checkAmmExist();
+
+    if (result) {
+      isInstalled().then(response => {
+        if (response.result.isInstalled) {
+          getAddress().then(response => {
+            const address = response.result?.address;
+            console.log('address', address);
+
+            const transaction = {
+              TransactionType: 'AMMDeposit',
+              Account: address,
+              Amount: ((xrpAddValue ?? 0) * 1e6).toString(),
+              Amount2: {
+                currency: 'MOI',
+                issuer: 'rPEQacsbfGADDHb6wShzTZ2ajByQFPdY3E',
+                value: (moaiAddValue ?? 0).toString(),
+              },
+              Asset: {
+                currency: 'XRP',
+              },
+              Asset2: {
+                currency: 'MOI',
+                issuer: 'rPEQacsbfGADDHb6wShzTZ2ajByQFPdY3E',
+              },
+              Fee: '100',
+              Flags: 1048576, // tfTwoAsset
+            };
+            submitTransaction({ transaction }) // ignore warning
+              .then(response => {
+                console.log('Transaction hash: ', response.result?.hash);
+              })
+              .catch(error => {
+                console.error('Transaction submission failed', error);
+              });
+          });
+        }
+      });
+    }
+  };
+
+  const handleSwapXRP = async () => {
+    const result = await checkAmmExist();
+
+    if (result) {
+      isInstalled().then(response => {
+        if (response.result.isInstalled) {
+          getAddress().then(response => {
+            const address = response.result?.address;
+            console.log('address', address);
+
+            const transaction = {
+              TransactionType: 'OfferCreate',
+              Account: address,
+              Flags: 0,
+              TakerGets: {
+                currency: 'MOI',
+                issuer: 'rPEQacsbfGADDHb6wShzTZ2ajByQFPdY3E',
+                value: (moaiToValue ?? 0).toString(),
+              },
+              TakerPays: ((xrpFromValue ?? 0) * 1e6).toString(),
+              Fee: '20',
+            };
+            submitTransaction({ transaction }) // ignore warning
+              .then(response => {
+                console.log('Transaction hash: ', response.result?.hash);
+              })
+              .catch(error => {
+                console.error('Transaction submission failed', error);
+              });
+          });
+        }
+      });
+    }
+  };
+
+  const handleSwapMOAI = async () => {
+    const result = await checkAmmExist();
+
+    if (result) {
+      isInstalled().then(response => {
+        if (response.result.isInstalled) {
+          getAddress().then(response => {
+            const address = response.result?.address;
+            console.log('address', address);
+
+            const transaction = {
+              TransactionType: 'OfferCreate',
+              Account: address,
+              Flags: 0,
+              TakerGets: ((xrpToValue ?? 0) * 1e6).toString(),
+              TakerPays: {
+                currency: 'MOI',
+                issuer: 'rPEQacsbfGADDHb6wShzTZ2ajByQFPdY3E',
+                value: (moaiFromValue ?? 0).toString(),
+              },
+              Fee: '20',
+            };
+            submitTransaction({ transaction }) // ignore warning
+              .then(response => {
+                console.log('Transaction hash: ', response.result?.hash);
+              })
+              .catch(error => {
+                console.error('Transaction submission failed', error);
+              });
+          });
+        }
+      });
+    }
+  };
+
   return (
     <>
       <Wrapper>
@@ -68,7 +190,7 @@ const XrplTestPage = () => {
           <ContentWrapper>
             <Title>XRPL TEST</Title>
             {/*
-                Provide LP
+                Create AMM
              */}
             <ButtonWrapper>
               <InputWrapper>
@@ -94,8 +216,79 @@ const XrplTestPage = () => {
             </ButtonWrapper>
 
             {/*
+                Provide Liquidity
+             */}
+            <ButtonWrapper>
+              <InputWrapper>
+                <InputInnerWrapper>
+                  <InputTitle>XRP</InputTitle>
+                  <InputTextField
+                    value={xrpAddValue}
+                    onChange={e => setXrpAddValue(Number(e.target.value))}
+                  ></InputTextField>
+                </InputInnerWrapper>
+                <InputInnerWrapper>
+                  <InputTitle>MOAI</InputTitle>
+                  <InputTextField
+                    value={moaiAddValue}
+                    onChange={e => setMoaiAddValue(Number(e.target.value))}
+                  ></InputTextField>
+                </InputInnerWrapper>
+              </InputWrapper>
+              <ButtonPrimaryMedium
+                text="Provide Liquidity"
+                onClick={() => handleProvideLP()}
+              ></ButtonPrimaryMedium>
+            </ButtonWrapper>
+
+            {/*
                 Swap
             */}
+            <ButtonWrapper>
+              <InputWrapper>
+                <InputInnerWrapper>
+                  <InputTitle>From XRP</InputTitle>
+                  <InputTextField
+                    value={xrpFromValue}
+                    onChange={e => setXrpFromValue(Number(e.target.value))}
+                  ></InputTextField>
+                </InputInnerWrapper>
+                <InputInnerWrapper>
+                  <InputTitle>To MOAI</InputTitle>
+                  <InputTextField
+                    value={moaiToValue}
+                    onChange={e => setMoaiToValue(Number(e.target.value))}
+                  ></InputTextField>
+                </InputInnerWrapper>
+              </InputWrapper>
+              <ButtonPrimaryMedium
+                text="Swap XRP to MOAI"
+                onClick={() => handleSwapXRP()}
+              ></ButtonPrimaryMedium>
+            </ButtonWrapper>
+
+            <ButtonWrapper>
+              <InputWrapper>
+                <InputInnerWrapper>
+                  <InputTitle>From MOAI</InputTitle>
+                  <InputTextField
+                    value={moaiFromValue}
+                    onChange={e => setMoaiFromValue(Number(e.target.value))}
+                  ></InputTextField>
+                </InputInnerWrapper>
+                <InputInnerWrapper>
+                  <InputTitle>To XRP</InputTitle>
+                  <InputTextField
+                    value={xrpToValue}
+                    onChange={e => setXrpToValue(Number(e.target.value))}
+                  ></InputTextField>
+                </InputInnerWrapper>
+              </InputWrapper>
+              <ButtonPrimaryMedium
+                text="Swap MAOI to XRP"
+                onClick={() => handleSwapMOAI()}
+              ></ButtonPrimaryMedium>
+            </ButtonWrapper>
           </ContentWrapper>
         </InnerWrapper>
         <Footer />
