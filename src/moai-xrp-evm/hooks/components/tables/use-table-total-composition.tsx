fe1e80e80beeed967e64634e0a57e78bf0afd266
jihyune <rlawlcns96@gmail.com>
2023-10-05 10:22:49 +0900

    fix: balance error

--- src/moai-xrp-evm/hooks/components/tables/use-table-total-composition.tsx
+++ src/moai-xrp-evm/hooks/components/tables/use-table-total-composition.tsx
@@ -41,7 +41,7 @@ export const useTableTotalComposition = (poolId: Address) => {
       value: balance * price,
       currentWeight: (balance / poolBalance) * 100,
 
-      userBalance,
+      balance,
     };
   });
 
@@ -55,9 +55,7 @@ export const useTableTotalComposition = (poolId: Address) => {
       />
     ),
     weight: <TableColumn value={`${d.weight.toFixed(2)}%`} width={120} align="flex-end" />,
-    balance: (
-      <TableColumn value={`${formatNumber(d.userBalance, 2)}`} width={120} align="flex-end" />
-    ),
+    balance: <TableColumn value={`${formatNumber(d.balance, 2)}`} width={120} align="flex-end" />,
     value: <TableColumn value={`$${formatNumber(d.value, 2)}`} width={120} align="flex-end" />,
     currentWeight: (
       <TableColumn value={`${formatNumber(d.currentWeight, 2)}%`} width={120} align="flex-end" />
