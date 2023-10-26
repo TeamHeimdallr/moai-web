bd676939731e9084f51a4e76f826a85be6b5840f
ssk7808 <ssk7808@naver.com>
2023-10-26 12:03:20 +0900

    add: network alert popup

---
+++ src/components/popup/network-alert/_network-alert.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta } from '@storybook/react';
+import tw from 'twin.macro';
+
+import { ButtonPrimaryLarge } from '~/components/buttons';
+
+import { usePopup } from '~/hooks/components/use-popup';
+import { POPUP_ID } from '~/types';
+
+import NetworkAlertPopup from '.';
+
+const meta = {
+  title: 'Components/Popup/NetworkAlertPopup',
+  component: NetworkAlertPopup,
+  tags: ['autodocs'],
+} satisfies Meta<typeof NetworkAlertPopup>;
+
+export default meta;
+
+const Wrapper = tw.div``;
+
+export const NoButton = () => {
+  const { opened, open, close } = usePopup(POPUP_ID.NETWORK_ALERT);
+
+  return (
+    <Wrapper>
+      <ButtonPrimaryLarge text="open popup !" onClick={open} />
+      {opened && <NetworkAlertPopup onClickButton={close} />}
+    </Wrapper>
+  );
+};
