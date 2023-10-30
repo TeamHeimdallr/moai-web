94f63d4b2a29979e498ca3d8fc25a0d6f5170a4e
jihyune <rlawlcns96@gmail.com>
2023-10-30 15:18:37 +0900

    fix: unwrap tx

--- src/api/api-contract/_xrpl/token/approve.ts
+++ src/api/api-contract/_xrpl/token/approve.ts
@@ -1,5 +1,5 @@
 import { useMutation, useQuery } from '@tanstack/react-query';
-import { AccountLinesRequest } from 'xrpl';
+import { AccountLinesRequest, TrustSet } from 'xrpl';
 
 import { QUERY_KEYS } from '~/api/utils/query-keys';
 
@@ -53,7 +53,7 @@ export const useApprove = ({ currency, issuer, amount }: Props) => {
       issuer,
       value: (Number(amount) + Number(line?.balance ?? 0)).toFixed(6),
     },
-  };
+  } as TrustSet;
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const setTrustLines = async () => await xrp.submitTransaction(txRequest as any);
