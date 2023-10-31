63d607392d62589a4c768d3f52bd6dc02ca2ae48
jihyune <rlawlcns96@gmail.com>
2023-10-31 13:14:17 +0900

    feat: show connect wallet banner when network changed

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,31 @@
 import tw, { css, styled } from 'twin.macro';
 
+import { AlertBanner } from '~/components/alerts/banner';
+import { ButtonPrimarySmallBlack } from '~/components/buttons';
 import { Footer } from '~/components/footer';
 import { Gnb } from '~/components/gnb';
 
+import { usePopup } from '~/hooks/components';
+import { POPUP_ID } from '~/types';
+
+import { useBanner } from './hooks/components/alert-wallet/use-banner';
 import { LiquidityPoolLayout } from './layouts/layout-liquidity-pool';
 import { MainLayout } from './layouts/layout-main';
 
 const HomePage = () => {
+  const { opened } = usePopup(POPUP_ID.WALLET_ALERT);
+  const { text, connectWallet } = useBanner();
+
   return (
     <>
       <Wrapper>
-        <GnbWrapper>
+        <GnbWrapper banner={!!opened}>
+          {opened && (
+            <AlertBanner
+              text={text}
+              button={<ButtonPrimarySmallBlack text="Connect wallet" onClick={connectWallet} />}
+            />
+          )}
           <Gnb />
         </GnbWrapper>
         <InnerWrapper>
@@ -32,10 +47,15 @@ const Wrapper = tw.div`
 const InnerWrapper = tw.div`
   flex flex-col gap-40 pb-120
 `;
-
-const GnbWrapper = tw.div`
-  w-full h-80 absolute top-0 left-0 flex-center z-10
-`;
+interface DivProps {
+  banner?: boolean;
+}
+const GnbWrapper = styled.div<DivProps>(({ banner }) => [
+  tw`
+    w-full absolute top-0 left-0 flex-center flex-col z-10
+  `,
+  banner ? tw`h-140` : tw`h-80`,
+]);
 
 const ContentWrapper = styled.div(() => [
   tw`flex flex-col items-center gap-80 px-80`,
