ec965b3ec6b0760fd3da22f2a39202a6d98578ab
Sean IM <chaaaamni@gmail.com>
2023-10-31 11:48:06 +0900

    fix route config, update deploy scripts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { Navigate, Route, Routes as ReactRoutes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
+import { IS_LANDING } from '~/constants';
+
 import { ConnectWallet } from '~/components/connect-wallet';
 
 import { usePopup } from '~/hooks/components/use-popup';
@@ -9,26 +11,37 @@ import { useWalletTypeStore } from '~/states/contexts/wallets/wallet-type';
 import { POPUP_ID } from '~/types';
 
 import Home from './home';
-import LandingPage from './landing';
+import Landing from './landing';
 import Pool from './pool';
 import Swap from './swap';
 
 const Page = () => {
   const { opened: connectWalletOpened } = usePopup(POPUP_ID.CONNECT_WALLET);
+
   useConnectXrpl();
   const { evm, xrpl } = useWalletTypeStore();
 
   return (
     <>
       <ReactRoutes>
-        <Route path="/" element={<Home />} />
-        <Route path="/swap" element={<Swap />} />
-        <Route path="/pools/*" element={<Pool />} />
+        {IS_LANDING && (
+          <>
+            <Route path="/" element={<Landing />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </>
+        )}
 
-        <Route path="/landing" element={<LandingPage />} />
+        {!IS_LANDING && (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/swap" element={<Swap />} />
+            <Route path="/pools/*" element={<Pool />} />
 
-        <Route path="*" element={<Navigate to="/" replace />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </>
+        )}
       </ReactRoutes>
+
       <ToastContainer />
       {connectWalletOpened && <ConnectWallet evm={evm} xrpl={xrpl} />}
     </>
