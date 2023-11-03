e13550256c720306e3a3378490f5033c68dd1b86
lucky7323 <eunho.ambition@gmail.com>
2023-11-03 17:11:18 +0900

    feat: futurepassOf's enable

--- src/hooks/wallets/use-futurepass-of.ts
+++ src/hooks/wallets/use-futurepass-of.ts
@@ -1,4 +1,4 @@
-import { useAccount, useContractRead } from 'wagmi';
+import { Address, useAccount, useContractRead } from 'wagmi';
 
 import { EVM_CONTRACT_ADDRESS } from '~/constants';
 
@@ -6,23 +6,26 @@ import { NETWORK } from '~/types';
 
 import { FUTUREPASS_REGISTER_ABI } from '~/abi/futurepass-register';
 
+import { useNetwork } from '../contexts/use-network';
+
 interface Props {
   enabled?: boolean;
 }
 export const useFuturepassOf = ({ enabled }: Props) => {
   const { address: walletAddress } = useAccount();
+  const { isFpass } = useNetwork();
 
   const {
     data: _data,
     refetch,
     ...rest
   } = useContractRead({
-    address: EVM_CONTRACT_ADDRESS[NETWORK.THE_ROOT_NETWORK].FUTUREPASS_REGISTER,
+    address: EVM_CONTRACT_ADDRESS[NETWORK.THE_ROOT_NETWORK].FUTUREPASS_REGISTER as Address,
     abi: FUTUREPASS_REGISTER_ABI,
     functionName: 'futurepassOf',
     args: [walletAddress],
 
-    enabled: enabled && !!walletAddress,
+    enabled: enabled && !!walletAddress && isFpass,
   });
 
   const data = _data as `0x${string}` | undefined;
