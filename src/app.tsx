422180814ad1226cfaca7afc3050ec31b874b959
ssk7808 <ssk7808@naver.com>
2023-11-08 17:54:46 +0900

    add: i18n provider at app.tsx, translation files

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import { Suspense } from 'react';
+import { I18nextProvider } from 'react-i18next';
 import { BrowserRouter } from 'react-router-dom';
 import tw from 'twin.macro';
 
 import ReactQueryProvider from './hocs/hoc-react-query-provider';
 import Web3Provider from './hocs/hoc-web3-provider';
+import i18n from './locales/i18n';
 import Pages from './pages';
 
 const RouteWrapper = tw.main`relative w-full h-full min-w-1440`;
@@ -14,7 +16,9 @@ const App = () => {
         <ReactQueryProvider>
           <BrowserRouter>
             <RouteWrapper>
-              <Pages />
+              <I18nextProvider i18n={i18n}>
+                <Pages />
+              </I18nextProvider>
             </RouteWrapper>
           </BrowserRouter>
         </ReactQueryProvider>
