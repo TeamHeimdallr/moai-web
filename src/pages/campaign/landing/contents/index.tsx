4a775a3f149c492a1e8684a7a94a7a4f32aeb752
jihyune <rlawlcns96@gmail.com>
2023-11-15 13:37:09 +0900

    fix: svg shadow and bg position

--- src/pages/campaign/landing/contents/index.tsx
+++ src/pages/campaign/landing/contents/index.tsx
@@ -17,8 +17,8 @@ export const Contents = () => {
       <ContentWrapper>
         <Title>Activate your $XRP</Title>
         <LogoWrapper>
-          <Logo1 width={isMD ? 249 : 149} height={isMD ? 70 : 24} />
-          <Logo2 width={isMD ? 489 : 293} height={isMD ? 70 : 24} />
+          <Logo1 className="svg-shadow" width={isMD ? 249 : 149} height={isMD ? 70 : 24} />
+          <Logo2 className="svg-shadow" width={isMD ? 489 : 293} height={isMD ? 70 : 24} />
         </LogoWrapper>
         <TextMain>
           {
@@ -46,12 +46,12 @@ export const Contents = () => {
 
 const Wrapper = tw.div`
   w-full px-20 pt-112 pb-40 flex flex-col gap-20 text-neutral-100 bg-no-repeat bg-campaign bg-cover bg-center
-  sm:pt-96 md:(min-h-screen gap-40 pt-292 bg-left pb-20) xxl:px-80
+  sm:pt-96 md:(min-h-screen gap-40 pt-292 bg-left bg-top pb-20) xxl:px-80
 `;
 
 const ContentWrapper = tw.div`flex flex-col gap-24 w-320 md:w-780 xl:w-760 xxl:w-840`;
 const Title = tw.div`font-b-20`;
-const LogoWrapper = tw.div`flex gap-12 svg-shadow w-293 flex-col md:(flex-row gap-18 w-754)`;
+const LogoWrapper = tw.div`flex gap-12 w-293 flex-col md:(flex-row gap-18 w-754)`;
 const ButtonWrapper = tw.div`w-157`;
 const TextMain = tw.div`w-full font-r-14 md:(font-r-16 whitespace-pre-line)`;
 const InfoWrapper = tw.div`flex gap-20 flex-col md:(flex-row) xxl:(gap-40)`;
