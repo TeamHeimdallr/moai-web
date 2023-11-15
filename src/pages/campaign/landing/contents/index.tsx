dd0fd176d155ecce2ef86b6e94651369edd66740
jihyune <rlawlcns96@gmail.com>
2023-11-15 14:13:07 +0900

    fix: card width responsive

--- src/pages/campaign/landing/contents/index.tsx
+++ src/pages/campaign/landing/contents/index.tsx
@@ -49,15 +49,15 @@ const Wrapper = tw.div`
   sm:pt-96 md:(min-h-screen gap-40 pt-292 bg-left bg-top pb-20) xxl:px-80
 `;
 
-const ContentWrapper = tw.div`flex flex-col gap-24 w-320 md:w-780 xl:w-760 xxl:w-840`;
+const ContentWrapper = tw.div`flex flex-col gap-24 w-320 md:w-full lg:w-840`;
 const Title = tw.div`font-b-20`;
-const LogoWrapper = tw.div`flex gap-12 w-293 flex-col md:(flex-row gap-18 w-754)`;
+const LogoWrapper = tw.div`flex gap-12 flex-col md:(flex-row gap-18)`;
 const ButtonWrapper = tw.div`w-157`;
 const TextMain = tw.div`w-full font-r-14 md:(font-r-16 whitespace-pre-line)`;
-const InfoWrapper = tw.div`flex gap-20 flex-col md:(flex-row) xxl:(gap-40)`;
+const InfoWrapper = tw.div`flex gap-20 flex-col md:(flex-row) xl:(gap-40)`;
 const Info = tw.div`
-  w-320 px-24 py-20 flex flex-col gap-12 rounded-12 bg-neutral-100 bg-opacity-10 backdrop-blur-sm
-  md:gap-16 xl:(w-370) xxl:(w-400)
+  w-full px-24 py-20 flex flex-col gap-12 rounded-12 bg-neutral-100 bg-opacity-10 backdrop-blur-sm
+  md:gap-16
 `;
 const Label = tw.div`font-m-14 text-neutral-80 md:font-m-16`;
 const Text = tw.div`font-m-20 md:font-m-24`;
