1056ef7ddfd7c91820ce3c86836ff1443f4d95ac
Sean IM <chaaaamni@gmail.com>
2023-11-22 16:06:04 +0900

    implement gnb menu dim

--- src/components/menu-mobile/index.tsx
+++ src/components/menu-mobile/index.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import { css } from '@emotion/react';
 import tw, { styled } from 'twin.macro';
 
 import { IconCancel } from '~/assets/icons';
@@ -32,6 +33,7 @@ export const MobileMenu = ({ closeMenu }: MobileMenuProps) => {
 
   return (
     <>
+      <Dim onClick={handleCloseClick} />
       <Wrapper>
         <UpperWrapper>
           <HeaderWrapper>
@@ -68,12 +70,24 @@ export const MobileMenu = ({ closeMenu }: MobileMenuProps) => {
   );
 };
 
-const Wrapper = tw.div`fixed right-0 w-360 flex flex-col justify-between h-screen bg-neutral-10 z-11 `;
-const UpperWrapper = tw.div`flex flex-col gap-40`;
-const HeaderWrapper = tw.div`flex items-center justify-between px-20 py-16`;
-const MenuWrapper = tw.div`flex flex-col gap-24 px-20`;
-const LogoWrapper = tw.div`clickable h-20`;
-const ButtonWrapper = tw.div`flex-center gap-8`;
+const Wrapper = tw.div`
+  fixed right-0 top-0 w-360 flex flex-col justify-between h-screen bg-neutral-10 z-12
+`;
+const UpperWrapper = tw.div`
+  flex flex-col gap-40
+`;
+const HeaderWrapper = tw.div`
+  flex items-center justify-between px-20 py-16
+`;
+const MenuWrapper = tw.div`
+  flex flex-col gap-24 px-20
+`;
+const LogoWrapper = tw.div`
+  clickable h-20
+`;
+const ButtonWrapper = tw.div`
+  flex-center gap-8
+`;
 interface MenuProps {
   selected: boolean;
   disabled: boolean;
@@ -82,4 +96,14 @@ const Menu = styled.div(({ selected, disabled }: MenuProps) => [
   tw`w-fit text-white clickable font-b-16`,
   disabled ? tw`non-clickable text-neutral-60` : tw`hover:text-primary-60`,
   !disabled && selected && tw`text-primary-60`,
+]);
+
+const Dim = styled.div(() => [
+  tw`
+  fixed left-0 top-0 z-11 w-full h-full
+`,
+  css`
+    background: rgba(25, 27, 40, 0.6);
+    backdrop-filter: blur(4px);
+  `,
 ]);
