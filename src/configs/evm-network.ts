abd06f92a72a75765675366ad565a512d1116f66
Sean IM <chaaaamni@gmail.com>
2023-11-30 11:19:46 +0900

    implement add network

--- src/configs/evm-network.ts
+++ src/configs/evm-network.ts
@@ -60,6 +60,30 @@ export const theRootNetwork: Chain = defineChain(
       }
 );
 
+export const theRootNetworkForAdd = IS_MAINNET
+  ? {
+      chainId: '0x1df4',
+      chainName: 'The Root Network - Mainnet',
+      rpcUrls: ['https://root.rootnet.live/archive'],
+      nativeCurrency: {
+        name: 'XRP',
+        symbol: 'XRP',
+        decimals: 18,
+      },
+      blockExplorerUrls: ['https://explorer.rootnet.live/'],
+    }
+  : {
+      chainId: '0x1df8',
+      chainName: 'The Root Network - Porcini Testnet',
+      nativeCurrency: {
+        name: 'XRP',
+        symbol: 'XRP',
+        decimals: 18,
+      },
+      rpcUrls: ['https://porcini.rootnet.app/archive'],
+      blockExplorerUrls: ['https://explorer.rootnet.cloud/'],
+    };
+
 export const xrpEvmSidechain: Chain = defineChain({
   id: 1440002,
   name: 'XRP EVM Sidechain - Devnet',
@@ -85,3 +109,15 @@ export const xrpEvmSidechain: Chain = defineChain({
   },
   testnet: true,
 });
+
+export const xrpEvmSidechainForAdd = {
+  chainId: '0x15f902',
+  chainName: 'XRP EVM Sidechain - Devnet',
+  nativeCurrency: {
+    name: 'XRP',
+    symbol: 'XRP',
+    decimals: 18,
+  },
+  rpcUrls: ['https://rpc-evm-sidechain.xrpl.org'],
+  blockExplorers: ['https://evm-sidechain.xrpl.org/'],
+};
