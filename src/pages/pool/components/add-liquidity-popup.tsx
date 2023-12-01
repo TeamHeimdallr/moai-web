0a479a25567629d25dbe32aaed5a99cecc83e6f1
Sean IM <chaaaamni@gmail.com>
2023-12-01 17:19:36 +0900

    fix add lp approve logic

--- src/pages/pool/components/add-liquidity-popup.tsx
+++ src/pages/pool/components/add-liquidity-popup.tsx
@@ -185,8 +185,9 @@ export const AddLiquidityPopup = ({
     }
 
     if (tokenLength === 2) {
-      if (allowance2) return 3;
-      if (allowance1) return 2;
+      if (!allowance1) return 1;
+      if (!allowance2) return 2;
+      return 3;
     }
 
     return 1;
