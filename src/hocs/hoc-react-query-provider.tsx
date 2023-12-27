8c98a33df47ecd0f0a07cb54929222bb6af1bfa8
ssk7808 <ssk7808@naver.com>
2023-12-27 15:07:28 +0900

    add: add liquidity input, popup skeleton

--- src/hocs/hoc-react-query-provider.tsx
+++ src/hocs/hoc-react-query-provider.tsx
@@ -3,7 +3,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 interface Props {
   children: React.ReactNode;
 }
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 const ReactQueryProvider = ({ children }: Props) => {
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
