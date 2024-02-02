06d39df2195ef97c9b6f8d7cd1fe7c4ba3ea9f91
Sean IM <chaaaamni@gmail.com>
2024-02-02 16:50:09 +0900

    implement xrpl single deposit

--- src/api/api-contract/_xrpl/pool/add-liquidity.ts
+++ src/api/api-contract/_xrpl/pool/add-liquidity.ts
@@ -1,25 +1,45 @@
+import { useParams } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
 import { parseUnits, toHex } from 'viem';
 
+import { useGetPoolQuery } from '~/api/api-server/pools/get-pool';
+
 import { useNetwork } from '~/hooks/contexts/use-network';
 import { useConnectedWallet } from '~/hooks/wallets';
-import { getTokenDecimal } from '~/utils';
+import { getNetworkAbbr, getNetworkFull, getTokenDecimal } from '~/utils';
 import { ITokenComposition, NETWORK } from '~/types';
 
 import { useAccountInfo } from '../account/account-info';
 
 interface Props {
+  poolId: string;
   token1: ITokenComposition & { balance: number; amount: number };
   token2: ITokenComposition & { balance: number; amount: number };
   enabled?: boolean;
 }
-export const useAddLiquidity = ({ token1, token2, enabled }: Props) => {
-  const { isXrp } = useNetwork();
+export const useAddLiquidity = ({ poolId, token1, token2, enabled }: Props) => {
+  const { network } = useParams();
+  const { selectedNetwork, isXrp } = useNetwork();
+  const currentNetwork = getNetworkFull(network) ?? selectedNetwork;
+  const currentNetworkAbbr = getNetworkAbbr(currentNetwork);
 
   const { xrp } = useConnectedWallet();
   const { address, connectedConnector } = xrp;
 
-  const tokens = [token1, token2];
+  const tokens = [token1, token2].filter(t => !!t);
+  const isSingle = tokens.filter(t => t?.amount > 0)?.length === 1;
+
+  const { data: poolData } = useGetPoolQuery(
+    {
+      params: {
+        poolId,
+        networkAbbr: currentNetworkAbbr as string,
+      },
+    },
+    { enabled: !!poolId && !!currentNetworkAbbr, staleTime: 3000 }
+  );
+  const { pool } = poolData ?? {};
+  const { compositions } = pool || {};
 
   const getTxRequestAssets = () => {
     const xrp = tokens?.find(t => t.currency === 'XRP');
@@ -28,19 +48,30 @@ export const useAddLiquidity = ({ token1, token2, enabled }: Props) => {
       const asset1 = { currency: 'XRP' };
       const amount1 = Number(
         Number(
-          parseUnits((xrp.amount || 0).toFixed(6), getTokenDecimal(NETWORK.XRPL, 'XRP')).toString()
+          parseUnits((xrp?.amount || 0).toFixed(6), getTokenDecimal(NETWORK.XRPL, 'XRP')).toString()
         ).toFixed(5)
       ).toString(); // max decimal is 6
 
       const remain = tokens.filter(t => t.currency !== 'XRP')?.[0];
+      const remainToken = compositions?.filter(t => t.currency !== 'XRP')?.[0];
       const asset2 = {
-        issuer: remain.address || '',
-        currency: remain.currency,
+        issuer: remainToken?.address || '',
+        currency: remainToken?.currency || '',
       };
       const amount2 = {
         ...asset2,
-        value: Number(Number(remain.amount || 0).toFixed(5)).toString(),
+        value: Number(Number(remain?.amount || 0).toFixed(5)).toString(),
       };
+
+      if (isSingle) {
+        const token = tokens.find(t => t?.amount > 0);
+        const amount = token?.symbol === 'XRP' ? amount1 : amount2;
+        return {
+          Amount: amount,
+          Asset: asset1,
+          Asset2: asset2,
+        };
+      }
 
       return {
         Amount: amount1,
@@ -50,10 +81,26 @@ export const useAddLiquidity = ({ token1, token2, enabled }: Props) => {
       };
     }
 
-    const asset1 = { issuer: token1?.address || '', currency: token1?.currency || '' };
+    const asset1 = {
+      issuer: compositions?.[0]?.address || '',
+      currency: compositions?.[0]?.currency || '',
+    };
     const amount1 = { ...asset1, value: Number(Number(token1?.amount || 0).toFixed(5)).toString() };
-    const asset2 = { issuer: token2?.address ?? '', currency: token2?.currency || '' };
-    const amount2 = { ...asset2, value: Number(Number(token1?.amount || 0).toFixed(5)).toString() };
+    const asset2 = {
+      issuer: compositions?.[1]?.address ?? '',
+      currency: compositions?.[1]?.currency || '',
+    };
+    const amount2 = { ...asset2, value: Number(Number(token2?.amount || 0).toFixed(5)).toString() };
+
+    if (isSingle) {
+      const token = tokens.find(t => t?.amount > 0);
+      const amount = [amount1, amount2].find(a => a?.currency === token?.currency) || amount1;
+      return {
+        Amount: amount,
+        Asset: asset1,
+        Asset2: asset2,
+      };
+    }
 
     return {
       Amount: amount1,
@@ -67,7 +114,7 @@ export const useAddLiquidity = ({ token1, token2, enabled }: Props) => {
   const sequence = accountInfo?.account_data.Sequence;
 
   const txAssets = getTxRequestAssets();
-  const txRequest = {
+  const txRequestTwoAssets = {
     TransactionType: 'AMMDeposit',
     Account: address,
     ...txAssets,
@@ -75,9 +122,20 @@ export const useAddLiquidity = ({ token1, token2, enabled }: Props) => {
     Flags: connectedConnector === 'dcent' ? toHex(1048576 + 2147483648) : 1048576,
     Sequence: connectedConnector === 'dcent' ? sequence : undefined,
   };
+  const txRequestSingleAsset = {
+    TransactionType: 'AMMDeposit',
+    Account: address,
+    ...txAssets,
+    Fee: '100',
+    Flags: connectedConnector === 'dcent' ? toHex(524288 + 2147483648) : 524288,
+    Sequence: connectedConnector === 'dcent' ? sequence : undefined,
+  };
+  const txRequest = isSingle ? txRequestSingleAsset : txRequestTwoAssets;
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const submitTx = async () => await xrp.submitTransaction(txRequest as any);
+  const submitTx = async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return await xrp.submitTransaction(txRequest as any);
+  };
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const { data, isLoading, isSuccess, mutateAsync } = useMutation<any>(
--- src/api/api-contract/_xrpl/pool/calculate-add-liquidity.ts
+++ src/api/api-contract/_xrpl/pool/calculate-add-liquidity.ts
@@ -6,13 +6,16 @@ import { useGetPoolQuery } from '~/api/api-server/pools/get-pool';
 import { useXrpl } from '~/hooks/contexts';
 import { useNetwork } from '~/hooks/contexts/use-network';
 import { usePrevious } from '~/hooks/utils';
+import { calcBptOutAmountAndPriceImpact } from '~/utils';
 import { IXrplTxInfo } from '~/types';
+
+import { useLpTokenTotalSupply } from '../token/lp-token-supply';
 
 interface Props {
   amountsIn: number[];
   txHash?: string;
 }
-export const useCalculateAddLiquidity = ({ txHash }: Props) => {
+export const useCalculateAddLiquidity = ({ txHash, amountsIn }: Props) => {
   const { client, isConnected } = useXrpl();
   const prevHash = usePrevious<string | undefined>(txHash);
 
@@ -33,13 +36,15 @@ export const useCalculateAddLiquidity = ({ txHash }: Props) => {
     }
   );
   const { pool } = poolData || {};
-  const { address } = pool || {};
+  const { address, compositions, tradingFee } = pool || {};
 
   const txRequest = {
     command: 'tx',
     transaction: txHash as string,
     binary: false,
   };
+
+  const { lpTokenTotalSupply } = useLpTokenTotalSupply({ poolId: id as string, network });
 
   const { data: txRaw } = useQuery<IXrplTxInfo>(
     ['GET', 'XRPL', 'TX', txHash],
@@ -54,13 +59,21 @@ export const useCalculateAddLiquidity = ({ txHash }: Props) => {
   const current = node?.FinalFields?.LPTokenBalance?.value || 0;
   const prev = node?.PreviousFields?.LPTokenBalance?.value || 0;
 
+  const { bptOut: expectedBptOut } = calcBptOutAmountAndPriceImpact({
+    balances: compositions?.map(c => c.balance || 0) || [],
+    normalizedWeights: compositions?.map(c => c.currentWeight || 0) || [],
+    amountsIn,
+    bptTotalSupply: lpTokenTotalSupply,
+    swapFeePercentage: tradingFee || 0,
+  });
+
   if (!isXrp)
     return {
       bptOut: 0,
       priceImpact: 0,
     };
 
-  const bptOut = Number(current) - Number(prev);
+  const bptOut = Number(current) - Number(prev) || expectedBptOut || 0;
   return {
     bptOut,
     priceImpact: 0,
