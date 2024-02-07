eea89ea2f1f63a4b2b8ee7836fd9a3fdcb05b3f4
Sean IM <chaaaamni@gmail.com>
2024-02-07 13:18:57 +0900

    fix number formatter

--- src/utils/util-number.ts
+++ src/utils/util-number.ts
@@ -23,8 +23,9 @@ export const formatPercent = (num: number, decimal?: number) =>
 /**
  * parseNumberWithUnit(42e6) === 42M
  */
-const formatter = Intl.NumberFormat('en', { notation: 'compact' });
-export const formatNumberWithUnit = (num: number) => formatter.format(num);
+const formatter = (digit = 2) =>
+  Intl.NumberFormat('en', { notation: 'compact', maximumSignificantDigits: digit });
+export const formatNumberWithUnit = (num: number, digit?: number) => formatter(digit).format(num);
 
 /**
  * parseNumberWithComma(10000) === 10,000
@@ -40,17 +41,37 @@ export const formatNumber = (
   data?: number | string,
   decimal = 4,
   type: 'round' | 'floor' = 'round',
-  threshold: number = FORMAT_NUMBER_THRESHOLD
+  threshold: number = FORMAT_NUMBER_THRESHOLD,
+  fixedDecimal: number = 0
 ) => {
   const formattedNumber =
     type === 'round'
-      ? Number(formatFloat(Number(data || 0), decimal))
+      ? Number(formatFloat(Number(data || 0), decimal + 2))
       : formatFloor(Number(data || 0), decimal);
 
   const formattedWithUnit =
     formattedNumber > threshold
-      ? formatNumberWithUnit(formattedNumber)
+      ? formatNumberWithUnit(formattedNumber, decimal + 2 || 1)
       : formatNumberWithComma(formattedNumber);
+
+  if (fixedDecimal) {
+    const regex = /[a-zA-z]/;
+    const hasUnit = regex.test(formattedWithUnit);
+
+    if (hasUnit) {
+      // ['M', index: 6, input: '123.12M', groups: undefined]
+      const unitIndex = formattedWithUnit.search(regex);
+      const unit = formattedWithUnit.slice(unitIndex, unitIndex + 1);
+
+      const num = formattedWithUnit.slice(0, unitIndex);
+      const fixedNum = Number(num).toFixed(fixedDecimal);
+
+      return `${fixedNum}${unit}`;
+    }
+
+    const fixedNum = Number(formattedWithUnit).toFixed(fixedDecimal);
+    return fixedNum;
+  }
 
   return formattedWithUnit;
 };
