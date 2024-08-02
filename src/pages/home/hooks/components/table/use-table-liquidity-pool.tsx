a3af08259af015179a4718a5164cd45d0f531010
Sean IM <chaaaamni@gmail.com>
2024-08-02 18:40:44 +0900

    fix pool order

--- src/pages/home/hooks/components/table/use-table-liquidity-pool.tsx
+++ src/pages/home/hooks/components/table/use-table-liquidity-pool.tsx
@@ -69,12 +69,11 @@ export const useTableLiquidityPool = () => {
     },
     { enabled: true }
   );
-  const _pools = useMemo(
+  const pools = useMemo(
     () => (data?.pages?.flatMap(page => page.pools) || []) as IPoolList[],
     [data?.pages]
   );
   // TODO: 서버코드로 수정
-  const pools = [..._pools.filter(p => !!p.apr), ..._pools.filter(p => !p.apr)];
 
   const poolTokens = useMemo(
     () => uniqBy(data?.pages?.flatMap(page => page.poolTokens) || [], 'symbol'),
